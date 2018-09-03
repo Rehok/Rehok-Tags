4d00e998f78c64e08efe9bbd7f40304874681e59
Rehok <Rehok@users.noreply.github.com>
2018-09-03 15:11:26 +0100

    Minor Adjustments
    Update the shortenNumber function, Now people can edit the number of digits after the . with ease if they so wish, Added some more tags that ill sort out in future

--- rTags.lua
+++ rTags.lua
@@ -19,41 +19,37 @@ function rTag:NewTags()
 -- https://wago.io/NJKRRlT9W --
 -------------------------------
 
-local shortenNumber = function(number)
+local shortenNumber = function(number, significant)
     if type(number) ~= "number" then
         number = tonumber(number)
     end
     if not number then
         return
     end
-
+    
+    if type(significant) ~= "number" then
+        significant = tonumber(significant)
+    end
+    significant = significant or 3
+    
     local affixes = {
-        "",
         "k",
         "m",
-        "B",
+        "b",
+        "t",
     }
-
-    local affix = 1
-    local dec = 0
-    local num1 = math.abs(number)
-    while num1 >= 1000 and affix < #affixes do
-        num1 = num1 / 1000
-        affix = affix + 1
-    end
-    if affix > 1 then
-        dec = 2
-        local num2 = num1
-        while num2 >= 10 do
-            num2 = num2 / 10
-            dec = dec - 1
-        end
-    end
-    if number < 0 then
-        num1 = -num1
-    end
-
-    return string.format("%."..dec.."f"..affixes[affix], num1)
+    affixes[0] = ""
+    
+    local log, floor, max, abs = math.log, math.floor, math.max, math.abs
+    
+    local powerTen = floor(log(abs(number)) / log(10)) --get the log base 10
+    powerTen = powerTen < 0 and 0 or powerTen --catch negative powers for numbers with an absolute value below 1
+    local affix = floor(powerTen / 3) --every third power of ten (so thousands) results in a new affix
+    local divNum = number / 1000^affix --get the "new" number by division with the floored amounts
+    local before = powerTen%3 + 1 --determine how many digits before the .
+    local after = max(0, significant - before) --and how many digits after
+    
+    return string.format(string.format("%%.%df%s", after, affixes[affix]), divNum)
 end
 
 -- Displays CurrentHP | Percent --(2.04B | 100)--
@@ -66,12 +62,31 @@ _G["ElvUF"].Tags.Methods['health:current-percent-r'] = function(unit)
 	local CurrentHealth = UnitHealth(unit)
 	local CurrentPercent = (UnitHealth(unit)/UnitHealthMax(unit))*100
 	if CurrentPercent > 1 then
-		return shortenNumber(CurrentHealth) .. " | " .. Round(CurrentPercent)
+		return shortenNumber(CurrentHealth, 2) .. " | " .. Round(CurrentPercent)
 	else
-		return shortenNumber(CurrentHealth) .. " | " .. format("%.1f", CurrentPercent)
+		return shortenNumber(CurrentHealth, 2) .. " | " .. format("%.1f", CurrentPercent)
 	end
 	end
 end
+
+-- Displays CurrentHP - Percent --(2.04B - 100), This is so i don't need 2 tags and display dead - dead --
+_G["ElvUF"].Tags.Events['health:current-percent-rClean'] = "UNIT_HEALTH_FREQUENT UNIT_MAXHEALTH UNIT_CONNECTION PLAYER_FLAGS_CHANGED"
+_G["ElvUF"].Tags.Methods['health:current-percent-rClean'] = function(unit)
+	local status = UnitIsDead(unit) and L["Dead"] or UnitIsGhost(unit) and L["Ghost"] or not UnitIsConnected(unit) and L["Offline"]
+		if (status) then
+			return status
+		else
+	local CurrentHealth = UnitHealth(unit)
+	local CurrentPercent = (UnitHealth(unit)/UnitHealthMax(unit))*100
+	if CurrentPercent <= 99.99 then
+		return shortenNumber(CurrentHealth, 2) .. " - " .. Round(CurrentPercent)
+	else
+		return shortenNumber(CurrentHealth, 2)
+	end
+	end
+end
+
+
 
 -- Displays current HP --(2.04B, 2.04M, 204k, 204)--
 _G["ElvUF"].Tags.Events['health:current-r'] = "UNIT_HEALTH_FREQUENT UNIT_MAXHEALTH UNIT_CONNECTION PLAYER_FLAGS_CHANGED"
@@ -81,7 +96,7 @@ _G["ElvUF"].Tags.Methods['health:current-r'] = function(unit)
 		      	return status
 		else
 	local CurrentHealth = UnitHealth(unit)
-		return shortenNumber(CurrentHealth)
+		return shortenNumber(CurrentHealth, 2)
 	end
 end
 
@@ -94,39 +109,47 @@ _G["ElvUF"].Tags.Methods['health:percent-r'] = function(unit)
 		return status
 	else
 	local CurrentPercent = (UnitHealth(unit)/UnitHealthMax(unit))*100
-		if CurrentPercent > 1 then
+		if CurrentPercent <= 99.99 then
 			return Round(CurrentPercent)
-		else
-			return format("%.1f", CurrentPercent)
 		end
 	end
 end
 
 
- -- Displays CurrenPower | Percent --(2.04B | 100)--
+ -- Displays CurrentPower | Percent --(2.04B | 100)--
 _G["ElvUF"].Tags.Events['power:current-percent-r'] = "UNIT_DISPLAYPOWER UNIT_POWER_UPDATE UNIT_POWER_FREQUENT"
 _G["ElvUF"].Tags.Methods['power:current-percent-r'] = function(unit)
 	local CurrentPower = UnitPower(unit)
 	local CurrentPercent = (UnitPower(unit)/UnitPowerMax(unit))*100
   local PowerMax = UnitPowerMax(unit)
 			if CurrentPercent > 1 and  PowerMax > 0 then
-		return shortenNumber(CurrentPower) .. " | " .. Round(CurrentPercent)
+		return shortenNumber(CurrentPower, 0) .. " | " .. Round(CurrentPercent)
 	else
-		return shortenNumber(CurrentPower) .. " | " .. Round(CurrentPercent)
+		return shortenNumber(CurrentPower, 0) .. " | " .. Round(CurrentPercent)
 	end
 end
 
--- Displays current power --(2.04B, 2.04M, 204k, 204, 0)--
+-- Displays current power --(2b, 2m, 204k, 204, 0)--
 _G["ElvUF"].Tags.Events['power:current-r'] = "UNIT_DISPLAYPOWER UNIT_POWER_UPDATE UNIT_POWER_FREQUENT"
 _G["ElvUF"].Tags.Methods['power:current-r'] = function(unit)
 	local CurrentPower = UnitPower(unit)
 	if CurrentPower > 0 then -- Some mobs have -1 as power, Don"t show if they have this
-		return shortenNumber(CurrentPower)
+		return shortenNumber(CurrentPower, 0)
 	else
 		return ""
   end
 end
 
+-- Displays current power --(2.04b, 2.04m, 204k, 204, 0)--
+_G["ElvUF"].Tags.Events['power:current-r'] = "UNIT_DISPLAYPOWER UNIT_POWER_UPDATE UNIT_POWER_FREQUENT"
+_G["ElvUF"].Tags.Methods['power:current-r'] = function(unit)
+	local CurrentPower = UnitPower(unit)
+	if CurrentPower > 0 then -- Some mobs have -1 as power, Don"t show if they have this
+		return shortenNumber(CurrentPower, 2)
+	else
+		return ""
+  end
+end
 
  -- Displays Power Percent
 _G["ElvUF"].Tags.Events['power:percent-r'] = "UNIT_DISPLAYPOWER UNIT_POWER_UPDATE UNIT_POWER_FREQUENT"
